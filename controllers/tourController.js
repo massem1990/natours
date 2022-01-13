d1c59526614eb96ed0281014e09b0c7a57fda2c6
Mahmoud Assem <m.assem@hotmail.com>
2022-01-13 15:35:39 +0100

    Update controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -49,7 +49,7 @@ exports.createTour = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      status: 'fail to create',
       message: err,
     });
   }
